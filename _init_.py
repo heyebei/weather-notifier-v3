261e6989b61ef5f033f143633df9b3642875704e
heyebei <www.2449071977@qq.com>
2025-07-11 13:13:36 +0800

    2025年7月11日

--- _init_.py
+++ _init_.py
@@ -11,5 +11,5 @@ def send_email():
         send_email(token, from_user, [sender], f'{city} 天气通知', f"配置成功！\n\n{weather_info}")
     print("✅ 已向新用户发送天气确认邮件")
 print("正在等待地址配置...")
-time.sleep(300)
+
 send_email()
