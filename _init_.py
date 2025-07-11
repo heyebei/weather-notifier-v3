0f8284eda50ab2385b057407d2c3ab9645d51a34
heyebei <www.2449071977@qq.com>
2025-07-11 13:36:42 +0800

    riqi

--- _init_.py
+++ _init_.py
@@ -10,7 +10,7 @@ def send_email():
     from weather_mailer import send_email, get_access_token, get_weather
     token = get_access_token(client_id, client_secret, tenant_id)
     with open('email_city_map.json', 'r', encoding="utf-8") as f:
-        for sender, city in f.read().splitlines():
+        for sender, city in f:
             weather_info = get_weather(city, os.environ['WEATHER_API_KEY'])
             send_email(token, from_user, [sender], f'{city} 天气通知', f"配置成功！\n\n{weather_info}")
     print("✅ 已向新用户发送天气确认邮件")
